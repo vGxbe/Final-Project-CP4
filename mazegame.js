e3e9ee3e79e5740f90e4411fc278fa61e709e41e
vGxbe <44502885+vGxbe@users.noreply.github.com>
2019-05-09 10:24:12 -0400

    Update mazegame.js

--- mazegame.js
+++ mazegame.js
@@ -1,5 +1,18 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+//The game board 1 = walls, 0 = free space, -1 = the goal, 2 = key
+var board = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+  [0, 0, 0, 2, 1, 1, 0, 0, 1, 0],
+  [0, 1, 1, 0, 0, 0, 1, 0, 1, 0],
+  [0, 0, 1, 1, 1, 1, 1, 0, 1, 0],
+  [1, 0, 1, 0, 0, 0, 1, 0, 1, 0],
+  [1, 0, 1, 0, 1, 0, 1, 0, 0, 0],
+  [1, 0, 1, 0, 1, 0, 0, 1, 1, 0],
+  [-1, 0, 1, 0, 1, 1, 0, 0, 0, 0]
+];
 
 ctx.beginPath();
 ctx.arc(40,40,40,0,2*Math.PI);
@@ -12,5 +25,7 @@ ctx.lineWidth = 5;
 ctx.strokeStyle = "gold";
 ctx.moveTo(0,80);
 ctx.lineTo(40, 120);
+ctx.moveTo(0, 120);
+ctx.lineTo(40,80);
 ctx.stroke();
 ctx.closePath();
