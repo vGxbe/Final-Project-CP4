702d5cc2b19522fdf1d4359f5743d3ec8013657c
vGxbe <44502885+vGxbe@users.noreply.github.com>
2019-05-12 11:56:25 -0400

    Update mazegame.js

--- mazegame.js
+++ mazegame.js
@@ -3,7 +3,7 @@ var ctx = canvas.getContext("2d");
 //The game board 1 = walls, 0 = free space, -1 = the goal, 2 = key
 var board = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
   [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
   [0, 0, 0, 2, 1, 1, 0, 0, 1, 0],
   [0, 1, 1, 0, 0, 0, 1, 0, 1, 0],
@@ -13,45 +13,55 @@ var board = [
   [1, 0, 1, 0, 1, 0, 0, 1, 1, 0],
   [-1, 0, 1, 0, 1, 1, 0, 0, 0, 0]
 ];
+var player = {
+  x: 0,
+  y: 0,
+};
 var width = canvas.width;
 var blockSize = width / board.length;
 
-function drawWall(){
+function drawWall(x, y) {
   ctx.beginPath();
-  ctx.rect(x*blockSize,y*blockSize,blockSize,blockSize);
+  ctx.rect(x * blockSize, y * blockSize, blockSize, blockSize);
   ctx.fillStyle = "black";
   ctx.fill();
+  ctx.closePath();
+}
+
+function drawPlayer(){
+  var half = blockSize / 2;
+  ctx.beginPath;
+  ctx.arc(player.x * blockSize + half, player.y * blockSize + half, half, 0, 2 * Math.PI);
+  ctx.fillStyle = "blue";
+  ctx.fill();
+  ctx.closePath;
+}
+
+function drawGoal(x, y){
+  ctx.beginPath();
+  ctx.lineWidth = 5;
+  ctx.strokeStyle = "gold";
+  ctx.moveTo(x*blockSize, y*blockSize);
+  ctx.lineTo((x+1)*blockSize, (y+1)*blockSize);
+  ctx.moveTo(x*blockSize, (y+1)*blockSize);
+  ctx.lineTo((x+1)*blockSize, y*blockSize);
+  ctx.stroke();
   ctx.closePath();
 }
 
 function draw() {
   for (var y = 0; y < board.length; y++) {
     for (var x = 0; x < board[y].length; x++) {
-      if (board[y][0] == 1) {
-        drawWall(0, y);
-      } else if (board[y][0] == -1) {
-        //draw Goal
-      } else if (board[y][0] == 2) {
-        // draw Key
+      if (board[y][x] == 1) {
+        drawWall(x, y);
+      } else if (board[y][x] == -1) {
+        drawGoal(x, y);
+      } else if (board[y][x] == 2) {
+        //draw Key
       }
     }
   }
+  drawPlayer();
 }
 
 draw();
-
-ctx.beginPath();
-ctx.arc(40,40,40,0,2*Math.PI);
-ctx.fillStyle = "blue";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.lineWidth = 5;
-ctx.strokeStyle = "gold";
-ctx.moveTo(0,80);
-ctx.lineTo(40, 120);
-ctx.moveTo(0, 120);
-ctx.lineTo(40,80);
-ctx.stroke();
-ctx.closePath();
